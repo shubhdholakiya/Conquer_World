233c2c23120295874c1fe4230b17dfea2d789ae7
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-09-22 12:51:42 +0530

    Add files via upload

---
+++ Python Tutorials/03_Strings.py
@@ -0,0 +1,24 @@
+single = 'conquer world'
+double = 'shubh'
+triple = '''shubh dholakiya'''
+
+
+statement = "This is Shubh's Pen."
+statement_01 = 'This is Shubh"s Pen.'
+statement_triple = '''This is Shubh"s 'Pen'.'''
+print(statement_triple)
+# statement_showing_error = 'This is Shubh"s Pen.'
+# print(statement_showing_error)
+
+
+# Concatenation of Strings
+# meaning: Addition
+
+first_name = "Shubh"
+last_name = "Dholakiya"
+
+print(first_name + " " +last_name)
+
+age = "20"
+name = "shubh"
+print(name + "=" + age)
