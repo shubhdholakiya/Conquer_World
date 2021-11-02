a71c37543be59d3067bd1721be2720eaadf99586
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-11-02 19:52:19 +0530

    Add files via upload

---
+++ Python Tutorials/04_variables.py
@@ -0,0 +1,15 @@
+alive = 69
+kills = 12
+name_of_the_player = "Shubh"
+alive_or_not = True
+
+cw_subs = 3.82
+
+cw_subs = 150
+
+print(str(cw_subs) + "K subscribers")
+
+age = 52
+Age = 12
+print(age)
+print(Age)
