6d1543d7fd9a73e358658037f67aac619350c8ac
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-11-07 13:52:50 +0530

    Add files via upload

---
+++ Python Tutorials/06_comp_op.py
@@ -0,0 +1,10 @@
+# = assigning a value
+# != not equal to
+# == comparing values and return True if both are same
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater than or equal to
+
+print("Conquer World" == "Conquer world")  # return False as "W" in second conquer world string is small
+print(5 != 6)
