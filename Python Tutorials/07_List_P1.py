91e6813c018e8a430bc46d8427777aa0e832ed59
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-11-28 14:02:40 +0530

    Add files via upload

---
+++ Python Tutorials/07_List_P1.py
@@ -0,0 +1,10 @@
+twelfth_A =["shubh", "rahul", "manan"]
+twelfth_B =["shreya", "riya", "priya"]
+
+# print(twelfth_A[0])
+# print(twelfth_B[2])
+
+# print("chirag" in twelfth_B)
+
+twelfth = twelfth_A + twelfth_B
+print(len(twelfth))
