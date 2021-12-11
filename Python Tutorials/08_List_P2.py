ebd205c29e89423b5ce92f74a5f9e035fb2f7aec
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-12-11 21:38:49 +0530

    Add files via upload

---
+++ Python Tutorials/08_List_P2.py
@@ -0,0 +1,25 @@
+group_1 = ["Shreya", "Dev", "Priya", "Vishnu"]
+# print(group_1)
+
+group_1.append("Anand")
+# print(group_1)
+
+group_1.insert(4, "Riya")
+# print(group_1)
+
+vowel = ["a", "e", "i", "o", "m"]
+# print(vowel)
+vowel[4] = "u"
+# print(vowel)
+
+print(vowel.index("i"))
+
+# print(vowel[0:4])
+
+# print(vowel[0:3:2])
+
+vowel.remove("i")
+# print(vowel)
+
+vowel.pop()
+print(vowel)
