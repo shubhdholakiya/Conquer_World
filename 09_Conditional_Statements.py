757a721771d6e027b0d6bf0e693c1701b09f22e5
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-12-25 19:16:56 +0530

    Add files via upload

---
+++ 09_Conditional_Statements.py
@@ -0,0 +1,10 @@
+income = int(input("Enter your Monthly Income: "))
+
+if (income >= 20000):
+    print("You did it! Now you can make a Girlfriend.")
+
+elif (income > 10000):
+    print("Try to find a girl with less Expectation! (Impossible")
+
+else:
+    print("Work harder, bro! Start to learn to Python from #EPICPY.")
