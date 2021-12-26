557c5a6832c61d7e409c534ad6eda9f604ff39da
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-12-26 22:59:54 +0530

    Update main.py

--- Python Projects/3. Currency Converter/main.py
+++ Python Projects/3. Currency Converter/main.py
@@ -3,12 +3,12 @@ from forex_python.bitcoin import BtcConverter
 
 c = CurrencyRates()
 curr = CurrencyCodes()
-# print(curr.get_symbol("INR"))
+# print(curr.get_symbol("INR")) # Print the Symbol of given currency.
 
-# print(c.get_rate("INR", "USD"))
+# print(c.get_rate("INR", "USD")) # Print rate of first currency in terms of second currency.
 
-# print(c.convert("USD", "INR", 20))
+# print(c.convert("USD", "INR", 20)) # Convert given amount from one curreny to another.
 
 
 b = BtcConverter()
-print(b.get_latest_price("INR"))
+print(b.get_latest_price("INR")) # Print rate of BTC in INR.
